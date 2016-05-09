b82ddb04df6e7f5413816304ff52988abfbf35e4
David Kosorin <david@kosorin.net>
2016-05-09 17:21:00 +0200

    GlobalSupressions.cs soubor.

--- OneDo/GlobalSuppressions.cs
+++ OneDo/GlobalSuppressions.cs
@@ -1,7 +1,8 @@
 ﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0113:Redundant comma in array initializer", Justification = "<Pending>", Scope = "member", Target = "~M:OneDo.Services.DataService.DesignDataService.LoadAsync~System.Threading.Tasks.Task")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0113:Redundant comma in array initializer", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language", "CSE0003:Use expression-bodied members", Justification = "<Pending>")]
