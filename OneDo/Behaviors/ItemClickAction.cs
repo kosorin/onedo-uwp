30da5a86ad12000e5168e29e1c0dd4a441fc6e8f
David Kosorin <david@kosorin.net>
2016-05-22 00:32:56 +0200

    Akce ItemClickAction po kliknutí na item (např. v ListView) zavolá Command s příslušným parametrem

---
+++ OneDo/Behaviors/ItemClickAction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OneDo.Behaviors
+{
+    public class ItemClickAction : DependencyObject, IAction
+    {
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(ItemClickAction), new PropertyMetadata(null));
+
+
+        public object Execute(object sender, object parameter)
+        {
+            var clickedItem = (parameter as ItemClickEventArgs)?.ClickedItem;
+            if (clickedItem != null)
+            {
+                Command?.Execute(clickedItem);
+            }
+            return null;
+        }
+    }
+}
