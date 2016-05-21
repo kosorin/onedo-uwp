048878a5f60961383d7bfc34cebdaf196aac4643
David Kosorin <david@kosorin.net>
2016-05-21 17:02:33 +0200

    TodoValidator s pravidlama pro Id a Title

---
+++ OneDo.Model/Business/Validation/TodoValidator.cs
@@ -0,0 +1,15 @@
+﻿using OneDo.Model.Data.Objects;
+using System;
+using System.Collections.Generic;
+
+namespace OneDo.Model.Business.Validation
+{
+    public class TodoValidator : Validator<Todo>
+    {
+        protected override IDictionary<string, Rule> Rules { get; } = new Dictionary<string, Rule>
+        {
+            [nameof(Todo.Id)] = new Rule(t => t.Id != Guid.Empty),
+            [nameof(Todo.Title)] = new Rule(t => !string.IsNullOrWhiteSpace(t.Title)),
+        };
+    }
+}
