ab77dfe4c36fa19b47df8e70e1e8026f297ac765
David Kosorin <david@kosorin.net>
2016-05-21 02:13:38 +0200

    Základ třídy BusinessBase

---
+++ OneDo.Model/Business/BusinessBase.cs
@@ -0,0 +1,14 @@
+﻿using OneDo.Model.Data;
+
+namespace OneDo.Model.Business
+{
+    public abstract class BusinessBase
+    {
+        public IDataProvider DataProvider { get; set; }
+
+        public BusinessBase(IDataProvider dataProvider)
+        {
+            DataProvider = dataProvider;
+        }
+    }
+}
