9161abce03e7bebdbf91738a5e814dce27b528aa
David Kosorin <david@kosorin.net>
2016-08-29 22:43:45 +0200

    ModalAttribute

---
+++ OneDo/View/ModalAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDo.View
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ModalAttribute : Attribute
+    {
+        public ModalAttribute(Type viewModelType)
+        {
+            ViewModelType = viewModelType;
+        }
+
+        public Type ViewModelType { get; }
+    }
+}
