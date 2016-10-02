a7fa693aeb48e5bc3c73123feeb6b219fbbc6076
David Kosorin <david@kosorin.net>
2016-10-02 12:49:07 +0200

    FlyoutExtensions

---
+++ OneDo/Common/UI/FlyoutExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+
+namespace OneDo.Common.UI
+{
+    public static class FlyoutExtensions
+    {
+        public static void ShowAttachedMenuFlyout(this FrameworkElement element, HoldingRoutedEventArgs args)
+        {
+            if (args.HoldingState == HoldingState.Started)
+            {
+                var flyout = FlyoutBase.GetAttachedFlyout(element) as MenuFlyout;
+                if (flyout != null)
+                {
+                    flyout.ShowAt(element, args.GetPosition(element));
+                }
+                args.Handled = true;
+            }
+        }
+
+        public static void ShowAttachedMenuFlyout(this FrameworkElement element, RightTappedRoutedEventArgs args)
+        {
+            var flyout = FlyoutBase.GetAttachedFlyout(element) as MenuFlyout;
+            if (flyout != null)
+            {
+                flyout.ShowAt(element, args.GetPosition(element));
+            }
+            args.Handled = true;
+        }
+    }
+}
