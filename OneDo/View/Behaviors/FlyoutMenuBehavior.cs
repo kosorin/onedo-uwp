9762088c360c0d1b92b58c2cb941727866a163b4
David Kosorin <david@kosorin.net>
2016-10-02 13:13:16 +0200

    FlyoutMenuBehavior

---
+++ OneDo/View/Behaviors/FlyoutMenuBehavior.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xaml.Interactivity;
+using OneDo.Common.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace OneDo.View.Behaviors
+{
+    public class FlyoutMenuBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject != AssociatedObject && !DesignMode.DesignModeEnabled)
+            {
+                AssociatedObject = associatedObject;
+                var element = AssociatedObject as FrameworkElement;
+                if (element != null)
+                {
+                    element.Holding += Holding;
+                    element.RightTapped += RightTapped;
+                }
+            }
+        }
+
+        public void Detach()
+        {
+            var element = AssociatedObject as FrameworkElement;
+            if (element != null)
+            {
+                element.Holding += Holding;
+                element.RightTapped += RightTapped;
+            }
+            AssociatedObject = null;
+        }
+
+        private void Holding(object sender, HoldingRoutedEventArgs e)
+        {
+            var element = AssociatedObject as FrameworkElement;
+            if (element != null)
+            {
+                element.RightTapped -= RightTapped;
+                FlyoutExtensions.ShowAttachedMenuFlyout(element, e);
+            }
+        }
+
+        private void RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            var element = AssociatedObject as FrameworkElement;
+            if (element != null)
+            {
+                FlyoutExtensions.ShowAttachedMenuFlyout(element, e);
+            }
+        }
+    }
+}
