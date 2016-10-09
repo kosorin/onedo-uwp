1b2aaa1eb38a2d6edb1a7a5c95a5d658ab01560f
David Kosorin <david@kosorin.net>
2016-10-09 03:59:21 +0200

    UI

--- OneDo/ViewModel/Items/NoteItemObject.cs
+++ OneDo/ViewModel/Items/NoteItemObject.cs
@@ -18,7 +18,7 @@ namespace OneDo.ViewModel.Items
 
         public bool HasText => !string.IsNullOrWhiteSpace(Text);
 
-        public bool IsOverdue => DateWithReminder != null && DateWithReminder < DateTime.Now;
+        public bool IsOverdue => (DateWithReminder ?? DateTime.MaxValue) < DateTime.Now;
 
         public DateTime? DateWithReminder => Reminder != null ? Date + Reminder : Date;
 
