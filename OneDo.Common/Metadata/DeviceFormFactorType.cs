503f101801de009412ba979c3cfa77b51ed233bf
David Kosorin <david@kosorin.net>
2016-10-18 02:22:50 +0200

    DeviceTypeHelper

---
+++ OneDo.Common/Metadata/DeviceFormFactorType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDo.Common.Metadata
+{
+    public enum DeviceFormFactorType
+    {
+        Phone,
+        Desktop,
+        Tablet,
+        IoT,
+        SurfaceHub,
+        Other,
+    }
+}
