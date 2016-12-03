5a9c75302ca6b7b064f386c8d9aa7ca22f788b01
David Kosorin <david@kosorin.net>
2016-12-03 22:22:34 +0100

    Testovací background task

--- OneDo/Core/BackgroundTasks/InProcessTestBackgroundTask.cs
+++ OneDo/Core/BackgroundTasks/InProcessTestBackgroundTask.cs
@@ -1,5 +1,6 @@
 ﻿using OneDo.Common.Logging;
 using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
 
 namespace OneDo.Core.BackgroundTasks
 {
@@ -9,7 +10,13 @@ namespace OneDo.Core.BackgroundTasks
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            Logger.Current.Info("Hello from in-process background task");
+            var deferral = taskInstance.GetDeferral();
+            var details = taskInstance.TriggerDetails as ToastNotificationHistoryChangedTriggerDetail;
+            if (details != null)
+            {
+                Logger.Current.Info($"Hello from in-process background task: {details.ChangeType}");
+            }
+            deferral.Complete();
         }
     }
 }
