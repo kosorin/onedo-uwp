b84e816cac4a1a9d38d02a30345554e13511592f
David Kosorin <david@kosorin.net>
2016-12-03 17:19:21 +0100

    Odstranění logování constructoru

--- OneDo/ViewModel/ExtendedViewModel.cs
+++ OneDo/ViewModel/ExtendedViewModel.cs
@@ -6,9 +6,11 @@ namespace OneDo.ViewModel
 {
     public abstract class ExtendedViewModel : ViewModelBase
     {
+#if false
         protected ExtendedViewModel()
         {
             Logger.Current.Trace($"{GetType().Name}.ctor");
         }
+#endif
     }
 }
