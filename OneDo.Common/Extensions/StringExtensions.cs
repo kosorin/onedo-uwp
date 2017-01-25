1970eed0cc3cb72c604ff3bf95ca77c4cb5fd498
David Kosorin <david@kosorin.net>
2017-01-26 00:13:58 +0100

    TrimNull string extension

---
+++ OneDo.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDo.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TrimNull(this string text)
+        {
+            return text?.Trim() ?? "";
+        }
+    }
+}
