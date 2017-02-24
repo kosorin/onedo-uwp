be45de94f8d379dd4e4ded7d5b44da3474e8522c
David Kosorin <david@kosorin.net>
2017-02-24 20:38:27 +0100

    Refactor: Equatable<T> is base class of ValueObject<T>

--- OneDo.Domain/Common/ValueObject.cs
+++ OneDo.Domain/Common/ValueObject.cs
@@ -1,4 +1,5 @@
-﻿using OneDo.Common.Data;
+﻿using OneDo.Common;
+using OneDo.Common.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace OneDo.Domain.Common
 {
-    public abstract class ValueObject<T> : IEquatable<T> where T : ValueObject<T>
+    public abstract class ValueObject<T> : Equatable<T> where T : ValueObject<T>
     {
         public static T Load(string json)
         {
@@ -22,53 +23,5 @@ namespace OneDo.Domain.Common
         {
             return Serialization.Serialize(this);
         }
-
-
-        public bool Equals(T other)
-        {
-            if (ReferenceEquals(other, null))
-            {
-                return false;
-            }
-            else
-            {
-                return EqualsCore(other);
-            }
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as T);
-        }
-
-        public override int GetHashCode()
-        {
-            return GetHashCodeCore();
-        }
-
-        public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
-        {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
-            {
-                return true;
-            }
-            else if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
-            {
-                return false;
-            }
-            else
-            {
-                return a.Equals(b);
-            }
-        }
-
-        public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
-        {
-            return !(a == b);
-        }
-
-        protected abstract bool EqualsCore(T other);
-
-        protected abstract int GetHashCodeCore();
     }
 }
